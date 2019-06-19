57fee33ea9fb37b0ed48180395e7f278d0a8c56c
Jan Krutisch <jan@krutisch.de>
2019-06-19 23:45:22 +0200

    Editing the wrong player.js. Twice.

--- website/js/player.js
+++ website/js/player.js
@@ -8,81 +8,103 @@ class DemoPlayer {
     this.element = element
     this.sourceNode = new MediaElementAudioSourceNode(this.context, {mediaElement: element})
     this.analyzer = context.createAnalyser()
+    this.analyzer.fftSize = 4096
     console.log(this.analyzer.frequencyBinCount)
     this.sourceNode.connect(this.analyzer)
     this.sourceNode.connect(this.context.destination)
-    this.playerType = element.getAttribute('data-player')
+    const player = element.getAttribute('data-player')
+    const [playerType, playerVariant] = player.split('-')
+    this.playerType = playerType
+    this.playerVariant = playerVariant
+    this.slideParent = this.element.closest('.remark-slide-content')
     this.insertUI()
-    this.slideshow.on('hideSlide', () => { this.active = false})
+    if (this.slideshow != null) {
+      this.slideshow.on('hideSlide', () => { this.active = false})
+    }
+    this.element.addEventListener('play', this.play.bind(this))
+    this.element.addEventListener('ended', this.ended.bind(this))
   }
   insertUI() {
     const wrapper = document.createElement('div')
     wrapper.className = `player player-${this.playerType}`
-
-
     const playButton = document.createElement('button')
     const buttonText = document.createTextNode("Play ▶")
     playButton.appendChild(buttonText)
     this.element.parentNode.insertBefore(wrapper, this.element.nextSibling)
     playButton.addEventListener('click', (e) => this.element.play())
-    this.element.addEventListener('play', this.play.bind(this))
+
+    this.scope = document.createElement('canvas')
+    this.scopeContext = this.scope.getContext('2d')
+    this.scope.width = 2048
+    this.scope.height = 1152
+
+    if (this.playerType !== 'simple') {
+      if (this.playerVariant === 'full' && this.slideParent != null) {
+        this.scope.className = "scope-full"
+        this.slideParent.appendChild(this.scope)
+      } else {
+        this.scope.className = "scope"
+        wrapper.appendChild(this.scope)
+      }
+    }
+
     if (this.playerType === 'scope') {
-      this.scope = document.createElement('canvas')
-      this.scope.className = "scope"
-      this.scope.width = 200
-      this.scope.height = 200
-      wrapper.appendChild(this.scope)
-      this.scopeContext = this.scope.getContext('2d')
       this.updateScope = this.updateScope.bind(this)
     }
     if (this.playerType === 'fft') {
-      this.scope = document.createElement('canvas')
-      this.scope.className = "scope"
-      this.scope.width = 600
-      this.scope.height = 200
-      wrapper.appendChild(this.scope)
-      this.scopeContext = this.scope.getContext('2d')
       this.updateFFT = this.updateFFT.bind(this)
     }
     wrapper.appendChild(playButton)
   }
   updateScope() {
     const dataArray = new Float32Array(this.analyzer.fftSize)
+    const halfHeight = this.scope.height / 2
     this.analyzer.getFloatTimeDomainData(dataArray)
     this.scopeContext.clearRect(0, 0, this.scope.width, this.scope.height)
-    this.scopeContext.strokeStyle = "#0f0"
+    if (this.playerVariant === 'full') {
+      this.scopeContext.strokeStyle = "rgba(0,128,0,0.5)"
+    } else {
+      this.scopeContext.strokeStyle = "#0f0"
+    }
     this.scopeContext.lineWidth = 4
     this.scopeContext.beginPath()
-    this.scopeContext.moveTo(0, 100)
+    this.scopeContext.moveTo(0, halfHeight)
     dataArray.forEach(function(v, i) {
       const x = i / this.analyzer.fftSize * this.scope.width
-      const halfHeight = this.scope.height / 2
       const y = v * halfHeight + halfHeight
       this.scopeContext.lineTo(x,y)
     }, this)
     this.scopeContext.stroke()
     if (this.active) {
       requestAnimationFrame(this.updateScope)
+    } else {
+      this.scopeContext.clearRect(0, 0, this.scope.width, this.scope.height)
     }
   }
   updateFFT() {
-    const dataArray = new Float32Array(this.analyzer.frequencyBinCount)
-    this.analyzer.getFloatFrequencyData(dataArray)
+    const dataArray = new Uint8Array(this.analyzer.frequencyBinCount)
+    this.analyzer.getByteFrequencyData(dataArray)
     // console.log(dataArray)
     this.scopeContext.clearRect(0, 0, this.scope.width, this.scope.height)
-    this.scopeContext.strokeStyle = "#0f0"
+    if (this.playerVariant === 'full') {
+      this.scopeContext.strokeStyle = "rgba(0,128,0,0.5)"
+    } else {
+      this.scopeContext.strokeStyle = "#0f0"
+    }
     this.scopeContext.lineWidth = 4
     this.scopeContext.beginPath()
     dataArray.forEach(function(v, i) {
       const x = i / this.analyzer.frequencyBinCount * this.scope.width
       const halfHeight = this.scope.height / 2
-      const y = this.scope.height - (v + 140) * 2
+      const y = this.scope.height - (this.scope.height * (v / 255))
       if (i == 0) { this.scopeContext.moveTo(x, y) }
       this.scopeContext.lineTo(x,y)
     }, this)
     this.scopeContext.stroke()
     if (this.active) {
       requestAnimationFrame(this.updateFFT)
+    } else {
+      this.scopeContext.clearRect(0, 0, this.scope.width, this.scope.height)
     }
   }
   play() {
@@ -96,6 +118,9 @@ class DemoPlayer {
         this.updateFFT()
       }
     }
+  }
+  ended() {
+    this.active = false
   }
 
 }
